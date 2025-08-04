dc83aa3bfadf6ee744ef83429cd4206a86467822
Hikko218 <heikor93@outlook.de>
2025-08-04 17:39:10 +0200

    Adjust port in backend

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -51,6 +51,6 @@ async function bootstrap() {
       max: 60,
     }),
   );
-  await app.listen(process.env.PORT || 3000, '0.0.0.0');
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 void bootstrap();
